45e9023cf97d890a128d3dfd9c3c14248ea65595
bailly.nthony <bailly.nthony@gmail.com>
2023-03-27 01:44:31 +0200

    add error in form vee-validate

--- frontend/vue-app/src/views/Login.vue
+++ frontend/vue-app/src/views/Login.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="login">
-    <h1>Se connecter</h1>
-    <div class="inputbox">
-      <input type="email" name="email" v-model="email" required />
-      <label>Email</label>
-    </div>
-    <span v-if="errors.email">{{ errors.email }}</span>
-    <div class="inputbox">
-      <input type="password" name="password" v-model="password" required />
-      <label>Mot de passe</label>
-    </div>
-    <span v-if="errors.password">{{ errors.password }}</span>
-    <div className="forget">
-      <label><input type="checkbox" v-model="rememberMe" />Se souvenir de moi <a href="#">Mot de passe oublié</a></label>
-    </div>
-    <button type="submit" @click="handleLogin">Se connecter</button>
+    <Form @submit="handleLogin" :validation-schema="schema">
+      <h1>Se connecter</h1>
+        <TextInput v-model="email" name="email" type="text" label="E-mail" placeholder="Your email address"
+          success-message="Got it, we won't spam you!" />
+        <TextInput v-model="password" name="password" type="password" label="Password" placeholder="Your password"
+          success-message="Nice and secure!" />
+      <div className="forget">
+        <label><input type="checkbox" v-model="rememberMe" />Se souvenir de moi <a href="#">Mot de passe
+            oublié</a></label>
+      </div>
+      <button class="submit-btn" type="submit" @click="handleLogin">Se connecter</button>
+    </Form>
   </div>
 </template>
 
@@ -23,11 +20,13 @@ import { ref } from 'vue'
 import { useAuthStore } from '@/stores/authStore'
 import router from '@/router'
 import * as Yup from 'yup'
+import { Form } from 'vee-validate';
+import TextInput from '../components/TextInput.vue';
 
 const schema = Yup.object().shape({
-  email: Yup.string().email('Invalid email address').required('Email is required'),
-  password: Yup.string().min(8, 'Password must be at least 8 characters').required('Password is required')
-})
+  email: Yup.string().email().required(),
+  password: Yup.string().min(6).required(),
+});
 
 const email = ref('')
 const password = ref('')
@@ -38,11 +37,6 @@ const authStore = useAuthStore()
 
 const handleLogin = async () => {
   try {
-    await schema.validate({
-      email: email.value,
-      password: password.value
-    }, { abortEarly: false })
-
     await authStore.login({
       email: email.value,
       password: password.value
@@ -69,56 +63,21 @@ const handleLogin = async () => {
 
 
 
-<style scoped>
+<style >
+:root {
+  --primary-color: #0071fe;
+  --error-color: #f23648;
+  --error-bg-color: #fddfe2;
+  --success-color: #21a67a;
+  --success-bg-color: #e0eee4;
+}
+
 .login {
   display: grid;
   align-content: center;
 }
-
-.inputbox {
-  position: relative;
-  margin: 20px 0 0;
-  width: 310px;
-  border-bottom: 2px solid #ffffff;
-}
-
-input:focus~label,
-input:valid~label {
-  top: -5px;
-}
-
-.inputbox input {
-  width: 100%;
-  height: 50px;
-  background: transparent;
-  border: none;
-  outline: none;
-  font-size: 1em;
-  padding: 0 35px 0 5px;
-  color: #fff;
-}
-
-.inputbox label {
-  position: absolute;
-  top: 55%;
-  left: 10px;
-  transform: translateY(-50%);
-  color: #ffffff;
-  font-size: 1em;
-  pointer-events: none;
-  transition: .5s;
-}
-
-.inputbox ion-icon {
-  position: absolute;
-  right: 8px;
-  color: #fff;
-  font-size: 1.2em;
-  top: 20px;
-}
-
 .forget {
-  margin: 10px 0 15px;
+  margin: 30px 0 15px;
   font-size: .9em;
   color: #fff;
   display: flex;
@@ -148,5 +107,8 @@ button {
   cursor: pointer;
   font-size: 1em;
   font-weight: 600;
+}
+.submit-btn:hover {
+  transform: scale(1.1);
 }
 </style>
